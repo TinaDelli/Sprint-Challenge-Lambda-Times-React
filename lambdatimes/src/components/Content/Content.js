169d1fa2bee9c1ec02a7a205cf80883879e52d8f
TinaDelli <cecurrier92@gmail.com>
2019-05-03 13:52:38 -0400

    tabs are changing proptypes are up and topbar is styled

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -10,7 +10,7 @@ export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
+      selected:'all',
       tabs: [],
       cards: []
     };
@@ -18,10 +18,17 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+    selected: tab
+    })
   };
 
   filterCards = () => {
@@ -37,8 +44,13 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all'){
+      return this.state.cards;
+    } else {
+     let cards = this.state.cards.filter(card => card.tab === this.state.selected);
+    return cards;
   };
+}
 
   render() {
     return (
@@ -48,7 +60,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+        tabs={this.state.tabs} 
+        selectedTab={this.state.selected}
+        selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
