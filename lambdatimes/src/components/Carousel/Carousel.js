22a7cce71f70652efc1c927490215ad6e9719f77
TinaDelli <cecurrier92@gmail.com>
2019-05-03 15:31:30 -0400

    played around with the carousel a bit more

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-      carousel: []
+      carousel: [],
+      index: 0,
+     isNext: true
     }
   }
   componentDidMount(){
     this.setState({
       carousel: carouselData,
-      index: 0
+
     })
   }
 
-  leftClick = (event) => {
-    // this.setState({
-    //   carousel: 
+  leftClick = () => {
+    let index = this.state.index,
+    length = this.state.carousel.length;
+
+      if( index < 1 ) {
+        index = length;
+      }
+
+      index = index - 1;
+
+      this.setState({
+        index: index,
+        isNext: false
+      });
   }
 
   rightClick = () => {
-
+    let index = this.state.index,
+        length = this.state.items.length - 1;
+    
+          if( index === length ) {
+            index = -1;
+          }
+          
+          index = index + 1;
+          
+          this.setState({
+            index: index,
+            isNext: true
+          });
   }
+ 
 
   selectedImage = () => {
-    return <img src={this.state.carousel.img} style={{display: 'block'}} />
+    
+    return <img src={this.state.carousel.index} style={{display: 'block'}} alt="carousel" />
   }
   
   render(){
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const TabsTabs = styled.div `
-  border-bottom: 1px solid lightgrey;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  width: 100%;
-  height: 47px;
-  background-color: #fff;
+// const TabsTabs = styled.div `
+//   border-bottom: 1px solid lightgrey;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: row;
+//   width: 100%;
+//   height: 47px;
+//   background-color: #fff;
 
-  @media (min-width: 1280px) {
-      width: 1280px;
-  }
-`
+//   @media (min-width: 1280px) {
+//       width: 1280px;
+//   }
+// `
 
 const Tabs = props => {
   return (
